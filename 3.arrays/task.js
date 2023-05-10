853471a0c07c268819555ec51ab2d5f32d98a598
Mr40peR <Mr40peR@yandex.ru>
2023-05-10 17:29:45 +0300

    edit task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length) {
+      return arr1.every((Element, Index) => (arr1[Element] === arr2[Element], arr1[Index] === arr2[Index]));
+    }  else {return false}
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, user) => {
+    acc += item;
+    if (index === user.length -1) {
+      return acc / user.length;
+    }
+    return acc
+  }, 0);
+   return result;
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
-function Student(name, gender, age) {
-  
+function Student (name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (Student.hasOwnProperty('marks') === true) {
+  	this.marks = marksToAdd;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (Student.hasOwnProperty('marks') === true || this.marks !== 0) {
+  	return this.marks.reduce((acc, item, index) => {
+  		acc += item;
+  		if (index === marks.length -1) {
+      	return acc / marks.length;
+    	}
+    	return acc;
+  	}, 0)
+  }
+  else {return 0;}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete Student.subject;
+  delete Student.marks;
+  this.excluded = reason;
 }
